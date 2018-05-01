29037c191ec0a9464fd412a413430fde2904c2fc
ericjohnson97 <eric.emj.johnson@gmail.com>
2018-04-30 21:41:40 -0500

    working much better now

--- src/lin_reg.cpp
+++ src/lin_reg.cpp
@@ -17,14 +17,16 @@ void chatterCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
  newpoint=*msg;
  newpoint.pose.position.z=2.9;
- if (newpoint.pose.position.x != points.front().pose.position.x && newpoint.pose.position.y != points.front().pose.position.y){
+ if (newpoint.pose.position.x != points.front().pose.position.x || newpoint.pose.position.y != points.front().pose.position.y){
     knew = 1;
     if (points.size() <= 5){
      points.push_front(newpoint);
+      cout<<"pop front" << endl;
     }
     else {
      points.pop_back();
      points.push_front(newpoint);
+     cout<<"pop back"<< endl;
     }
   }
 }
@@ -41,9 +43,9 @@ int main(int argc, char **argv)
   //points.push_front(buffer);
 
   ros::Subscriber sub = n.subscribe("roombaPose", 1, chatterCallback);
-  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("waypoint", 1000);
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("waypoint", 1);
 
-  ros::Rate loop_rate(1000);
+  ros::Rate loop_rate(10);
 
   int count = 0;
   while (ros::ok())
@@ -57,19 +59,18 @@ int main(int argc, char **argv)
           double nx = points[i].pose.position.x - points.back().pose.position.x;
           double ny = points[i].pose.position.y - points.back().pose.position.y;
           double nt = (points[i].header.stamp.sec + points[i].header.stamp.nsec * pow(10,-9)) - (points.back().header.stamp.sec + points.back().header.stamp.nsec * pow(10,-9));
-          
+          cout <<"nt: " << nt << endl;
           sx = sx + nx;
           sy = sy + ny;
           st = st + nt; 
-          sxt = sxt + nx  * nt;
+          sxt = sxt + nx * nt;
           syt = syt + ny * nt;
           sx2 = sx2 + nx * nx;
           sy2 = sy2 + ny * ny;
 
-          cout<<endl<<"point "<< points[i]<<endl;
+          //cout<<endl<<"point "<< points[i]<<endl;
        }
 
-       cout<<endl;
        cout<<"delta T in use: "<<st<<endl;
        cout<<"the back x point is "<<points.back().pose.position.x<<endl;
 
